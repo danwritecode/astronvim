a2d1e4dd88c2fa7c06e5eae8d4ca6e88bad77702
Dan Nelson <dnelson4993@outlook.com>
2022-08-19 15:39:41 -0400

    update config

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -27,7 +27,8 @@ local config = {
 
   -- Set colorscheme to use
   -- colorscheme = "default_theme",
-  colorscheme = "dracula",
+  -- colorscheme = "tokyonight",
+  colorscheme = "molokai",
 
   -- Override highlight groups in any theme
   highlights = {
@@ -67,10 +68,6 @@ local config = {
     "|     ___ ___    |  |  |_|_____ ",
     "|  |  | .'|   |  |  |  | |     |",
     "|____/|__,|_|_|    ___/|_|_|_|_|",
-  },
-
-  dracula_colors = {
-    bg = "#000000",
   },
 
   -- Default theme configuration
@@ -188,32 +185,20 @@ local config = {
           }
         end,
       },
+      {
+        "folke/tokyonight.nvim",
+        config = function() vim.g.tokyonight_style = "night" end,
+      },
+      ["jose-elias-alvarez/null-ls"] = { disable = true },
+      {
+        "eddyekofo94/gruvbox-flat.nvim",
+        config = function()
+          vim.o.background = "dark"
+          vim.g.gruvbox_flat_style = "hard"
+        end,
+      },
+      "tamelion/neovim-molokai"
     },
-    -- All other entries override the require("<key>").setup({...}) call for default plugins
-    ["null-ls"] = function(config) -- overrides `require("null-ls").setup(config)`
-      -- config variable is the default configuration table for the setup functino call
-      local null_ls = require "null-ls"
-      -- Check supported formatters and linters
-      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-      config.sources = {
-        -- Set a formatter
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-      }
-      -- set up null-ls's on_attach function
-      -- NOTE: You can remove this on attach function to disable format on save
-      config.on_attach = function(client)
-        if client.resolved_capabilities.document_formatting then
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            desc = "Auto format before save",
-            pattern = "<buffer>",
-            callback = vim.lsp.buf.formatting_sync,
-          })
-        end
-      end
-      return config -- return final config table to use in require("null-ls").setup(config)
-    end,
     treesitter = { -- overrides `require("treesitter").setup(...)`
       ensure_installed = { "lua" },
     },
